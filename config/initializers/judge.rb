6897320dbf521d6e4aff062af6a61b840d2eb4e4
seb1441 <sebastienauriault@gmail.com>
2018-08-19 21:20:46 -0400

    Initial Commit

---
+++ config/initializers/judge.rb
@@ -0,0 +1,7 @@
+# expose uniqueness attributes in Judge in here with the following syntax
+#
+Judge.configure do
+  expose Article, :slug
+end
+#   expose Model1, :attr
+#   expose Model2, :attr1, :attr2
